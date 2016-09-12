b15fbb2d7a27d95037973b51f9e11bef9be0aa91
dannyperry571 <dannyperry571@msn.com>
2016-09-12 17:24:32 +0100

    v2
    v2

--- plugin.video.Carrera/default.py
+++ plugin.video.Carrera/default.py
@@ -24,12 +24,11 @@ try:
 except:
     import simplejson as json
 import yt
-####################################PARTS TO EDIT#######################################
-ADDON_NAME = 'Carrera'           ### CHANGE THIS NEW NAME OF ADDON
-addon_id = 'plugin.video.Carrera' ### CHANGE THIS TO NEW FOLDER NAME - plugin.video.***   just change 'testpiece'
-Base_Url = 'http://herovision.x10host.com/carrera/'   ###Change this to the location of the folder/url where you will be storing all your files, must end with /
-Main_Menu_File_Name = 'main.php'    ### Change this to the name of the file that you wish to be your main menu when you open the addon in kodi
-search_filenames = ['sv1','teh','s5'] ### ADD YOUR PHP FILE NAMES IN HERE TO BE INCLUDED IN SEARCHES
+ADDON_NAME = 'Carrera'
+addon_id = 'plugin.video.Carrera'
+Base_Url = 'http://herovision.x10host.com/carrera/'
+Main_Menu_File_Name = 'main.php'
+search_filenames = ['sv1','teh','s5']
 ########################################################################################
 ### FAVOURITES SECTION IS NOT THIS AUTHORS CODE, I COULD NOT GET IT TO REMOVE FAVOURITES SO ALL CREDIT DUE TO THEM, SORRY IM NOT SURE WHERE IT CAME FROM BUT GOOD WORK :) ###
 
@@ -43,6 +42,7 @@ Dialog = xbmcgui.Dialog()
 addon_data = xbmc.translatePath('special://home/userdata/addon_data/'+addon_id+'/')
 favorites = os.path.join(addon_data, 'favorites.txt')
 watched = addon_data + 'watched.txt'
+source_file = Base_Url + 'source_file.php'
 debug = ADDON.getSetting('debug')
 if os.path.exists(addon_data)==False:
     os.makedirs(addon_data)
@@ -62,7 +62,9 @@ def Main_Menu():
         elif 'php' in url:
             Menu(name,url,1,icon,fanart,desc)
         elif name == '[COLORskyblue]S[COLORblue]earch[/COLOR]':
-         	Menu('[COLORskyblue]S[COLORblue]earch[/COLOR]',url,3,icon,fanart,desc)	
+         	Menu('[COLORskyblue]S[COLORblue]earch[/COLOR]',url,3,icon,fanart,desc)
+        elif name == '[COLORskyblue]i[COLORblue]dex[/COLOR]':
+            Menu('[COLORskyblue]O[COLORblue]nline Lists[/COLOR]',url,10,icon,fanart,desc)
         else:
             Play(name,url,2,icon,fanart,desc)
     setView('tvshows', 'Media Info 3')			
@@ -83,7 +85,81 @@ def Second_Menu(url):
         else:
             Play(name,url,2,icon,fanart,desc)
     setView('tvshows', 'Media Info 3')
-	
+
+def index_Menu():
+    #Menu('Favourites','',5,'','','','','')
+    Menu('List of Index\'s','',10,'','','')
+    #    Menu('Search','',6,ICON,FANART,'','','')
+# Menu('[COLORred]Press here to add a source url[/COLOR]	','',2,'','','','','')
+
+def Index_List():
+    OPEN = Open_Url(source_file)
+    Regex = re.compile('url="(.+?)">name="(.+?)"').findall(OPEN)
+    for url,name in Regex:
+            Menu(name,url,8,'','','')
+
+
+
+#####################################MAIN REGEX LOOP ###############################
+
+def Main_Loop(url):
+    HTML = Open_Url(url)
+    match = re.compile('<a href="(.+?)">(.+?)</a>').findall(HTML)
+    for url2,name in match:
+        url3 = url + url2
+        if '..' in url3:
+            pass
+        elif 'rar' in url3:
+            pass
+        elif 'jpg' in url3:
+            pass
+        elif 'vtx' in url3:
+            pass
+        elif 'srt' in url3:
+            pass
+        elif 'C=' in url2:
+            pass
+        elif '/' in url2:
+            Menu((name).replace('/',''),url3,8,ICON,FANART,'','','')
+        else:
+            Clean_name(name,url3)
+
+################################### TIDY UP NAME #############################
+
+def Clean_name(name,url3):
+    name1 = (name).replace('S01E','S01 E').replace('(MovIran).mkv','').replace('The.Walking.Dead','').replace('.mkv','').replace('Tehmovies.com.mkv','').replace('Nightsdl','').replace('Ganool','')
+    name2=(name1).replace('.',' ').replace(' (ParsFilm).mkv','').replace('_TehMovies.Com.mkv','').replace(' (SaberFun.IR).mkv','').replace('[UpFilm].mkv','').replace('(Bia2Movies)','')
+    name3=(name2).replace('.mkv','').replace('.Film2Movie_INFO.mkv','').replace('.HEVC.Film2Movie_INFO.mkv','').replace('.ParsFilm.mkv ','').replace('(SaberFunIR)','')
+    name4=(name3).replace('.INTERNAL.','').replace('.Film2Movie_INFO.mkv','').replace('.web-dl.Tehmovies.net.mkv','').replace('S01E06','S01 E06').replace('S01E07','S01 E07')
+    name5=(name4).replace('S01E08','S01 E08').replace('S01E09','S01 E09').replace('S01E10','S01 E10').replace('.Tehmovies.net','').replace('.WEBRip.Tehmovies.com.mkv','')
+    name6=(name5).replace('.mp4','').replace('.mkv','').replace('.Tehmovies.ir','').replace('x265HEVC','').replace('Film2Movie_INFO','').replace('Tehmovies.com.mkv','')
+    name7=(name6).replace(' (ParsFilm)','').replace('Tehmovies.ir.mkv','').replace('.480p',' 480p').replace('.WEBrip','').replace('.web-dl','').replace('.WEB-DL','')
+    name8=(name7).replace('.','').replace('.Tehmovies.com','').replace('480p.Tehmovies.net</',' 480p').replace('720p.Tehmovies.net','720p').replace('.480p',' 480p')
+    name9=(name8).replace('.480p.WEB-DL',' 480p').replace('.mkv','').replace('.INTERNAL.','').replace('720p',' 720p').replace('.Tehmovi..&gt;','').replace('.Tehmovies.net.mkv','')
+    name10=(name9).replace('..720p',' 720p').replace('.REPACK.Tehmovies..&gt;','').replace('.Tehmovies.com.mkv','').replace('.Tehmovies..&gt;','').replace('Tehmovies.ir..&gt;','')
+    name11=(name10).replace('Tehmovies.ne..&gt;','').replace('.HDTV.x264-mRs','').replace('...&gt;','').replace('.Tehmovies...&gt;','').replace('.Tehmovies.com.mp4','')
+    name12=(name11).replace('.Tehmovies.com.mp4','').replace('_MovieFarsi','').replace('_MovieFar','').replace('_com','').replace('&gt;','').replace('avi','').replace('(1)','')
+    name13=(name12).replace('(2)','').replace('cd 2','').replace('cd 1','').replace('-dos-xvid','').replace('divx','').replace('Xvid','').replace('DVD','').replace('DVDrip','')
+    name14=(name13).replace('DvDrip-aXXo','').replace('[','').replace(']','').replace('(','').replace(')','').replace('XviD-TLF-','').replace('CD1','').replace('CD2','')
+    name15=(name14).replace('CD3','').replace('mp4','').replace('&amp;','&').replace('HDRip','').replace('-','').replace('  ',' ').replace('xvid','').replace('1080p','')
+    name16=(name15).replace('1970','').replace('1971','').replace('1972','').replace('1973','').replace('1974','').replace('1975','').replace('1976','').replace('1977','')
+    name17=(name16).replace('1978','').replace('1979','').replace('1980','').replace('1981','').replace('1982','').replace('1983','').replace('1984','').replace('1985','')
+    name18=(name17).replace('1986','').replace('1987','').replace('1988','').replace('1989','').replace('1990','').replace('1991','').replace('1992','').replace('1993','')
+    name19=(name18).replace('1994','').replace('1995','').replace('1996','').replace('1997','').replace('1998','').replace('1999','').replace('2000','').replace('2001','')
+    name20=(name19).replace('2002','').replace('2003','').replace('2004','').replace('2005','').replace('2006','').replace('2007','').replace('2008','').replace('2009','')
+    name21=(name20).replace('2010','').replace('2011','').replace('2012','').replace('2013','').replace('2014','').replace('2015','').replace('2016','').replace('720p','')
+    name22=(name21).replace('360p','').replace('  ',' ').replace('BluRay','').replace('rip','').replace('WEBDL','').replace('s01','').replace('s02','').replace('S02','')
+    name23=(name22).replace('s03','').replace('s04','').replace('s05','').replace('s06','').replace('s07','').replace('s08','').replace('s09','').replace('S01','')
+    name24=(name23).replace('S03','').replace('S04',' ').replace('S05','').replace('S06','').replace('S07','').replace('S08','').replace('S09','').replace('E01','')
+    name25=(name24).replace('E02','').replace('E03','').replace('E04','').replace('E05','').replace('E06','').replace('E07','').replace('E08','').replace('E09','').replace('e01','')
+    name25=(name24).replace('e02','').replace('e03','').replace('e04','').replace('e05','').replace('e06','').replace('e07','').replace('e08','').replace('e09','').replace('e01','')
+    clean_name = name15
+    search_name = name25
+        #if ADDON.getSetting('Data')=='true':
+        # Imdb_Scrape(url3,clean_name,search_name)
+        #if ADDON.getSetting('Data')=='false':
+    Play(clean_name,url3,2,ICON,FANART,'','','')
+
 def Search():
     Search_Name = Dialog.input('Search', type=xbmcgui.INPUT_ALPHANUM)
     Search_Title = Search_Name.lower()
@@ -130,7 +206,6 @@ def Open_Url(url):
 def setView(content, viewType):
 	if content:
 	    xbmcplugin.setContent(int(sys.argv[1]), content)
-		
 		
 def Menu(name,url,mode,iconimage,fanart,description,showcontext=True,allinfo={}):
         u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
@@ -367,6 +442,11 @@ elif mode==5:
 elif mode==6:
     addon_log("getFavorites")
     getFavorites()
+elif mode == 7 : index_Menu()
+elif mode == 8 : Main_Loop(url)
+elif mode == 9 : Source_File()
+elif mode ==10 : Index_List()
 
+xbmcplugin.addSortMethod(int(sys.argv[1]), 1)
 		
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
